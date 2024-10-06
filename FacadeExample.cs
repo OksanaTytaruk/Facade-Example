0a58615e8e27e79eb5849a89aaccd92de809ce5e
OksanaTytaruk <cassieroyster@gmail.com>
2024-10-07 00:47:52 +0300

    Create FacadeExample.cs

---
+++ FacadeExample.cs
@@ -0,0 +1,50 @@
+using System;
+
+// Підсистема A
+public class SubsystemA
+{
+    public void OperationA()
+    {
+        Console.WriteLine("Subsystem A: Ready!");
+    }
+}
+
+// Підсистема B
+public class SubsystemB
+{
+    public void OperationB()
+    {
+        Console.WriteLine("Subsystem B: Ready!");
+    }
+}
+
+// Фасад
+public class Facade
+{
+    private SubsystemA _subsystemA;
+    private SubsystemB _subsystemB;
+
+    public Facade()
+    {
+        _subsystemA = new SubsystemA();
+        _subsystemB = new SubsystemB();
+    }
+
+    public void Operation()
+    {
+        Console.WriteLine("Facade initializes subsystems:");
+        _subsystemA.OperationA();
+        _subsystemB.OperationB();
+        Console.WriteLine("Facade: Subsystems initialized!");
+    }
+}
+
+// Клієнтський код
+class Program
+{
+    static void Main(string[] args)
+    {
+        Facade facade = new Facade();
+        facade.Operation();
+    }
+}
